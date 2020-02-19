9aae11fe959cdf214d08d50df8a77adbd9e45f5e
RESBI <resbi@redstone.link>
2020-02-19 17:05:07 +0800

    Update

--- slime.c
+++ slime.c
@@ -4,13 +4,13 @@
 void getSeed(long long *RndSeed, long long MinecraftSeed, int xPosition, int zPosition);
 
 int main() {
-  long long *RndSeed;
-  long long MinecraftSeed = -2013662552565141358;
+  long long *RndSeed; // The seed must be pointer type.
+  long long MinecraftSeed = -2013662552565141358; // You can replace it with your seed of Minecraft.
 
-  RndSeed = (long long *)malloc(sizeof(long long));
+  RndSeed = (long long *)malloc(sizeof(long long)); // There're some bugs if you delete this.
   
-  for (int x=0; x<16; x+=1) {
-    for (int z=0; z<16; z+=1) {
+  for (int x=0; x<16; x+=1) { // actuall x / 16
+    for (int z=0; z<16; z+=1) { // actuall z / 16
       getSeed(RndSeed, MinecraftSeed, x, z);
       setSeed(RndSeed);
   
@@ -18,10 +18,12 @@ int main() {
       //printf("RndSeed: %lld\n", RndSeed[0]);
       //printf("RandNum: %d\n", nextInt(10, RndSeed));
       if (nextInt(10, RndSeed) == 0) {
-	printf("x=%d\tz=%d\tis a slime chunk!\n", x, z);//x*16, z*16);
+	printf("x=%d\tz=%d\tis a slime chunk!\n", x*16, z*16); // Turn to the real location.
       }
     }
   }
+  
+  free(RndSeed); // Let it go.
   return 0;
 }
 
