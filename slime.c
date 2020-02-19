edead6a6e8e99943371ef8d0e8fcd6be5cdae252
Resbi <resbi@redstone.link>
2020-02-19 19:02:16 +0800

    Add files via upload

--- slime.c
+++ slime.c
@@ -4,21 +4,23 @@
 void getSeed(long long *RndSeed, long long MinecraftSeed, int xPosition, int zPosition);
 
 int main() {
-  long long *RndSeed; // The seed must be pointer type.
   long long MinecraftSeed = -2013662552565141358; // You can replace it with your seed of Minecraft.
-
+  long long *RndSeed; // The seed must be pointer type.
   RndSeed = (long long *)malloc(sizeof(long long)); // There're some bugs if you delete this.
+  int judge = 0;
   
-  for (int x=0; x<16; x+=1) { // actuall x / 16
-    for (int z=0; z<16; z+=1) { // actuall z / 16
+  for (int x=0; x<8; x+=1) { // actuall x / 16
+    for (int z=0; z<8; z+=1) { // actuall z / 16
       getSeed(RndSeed, MinecraftSeed, x, z);
+      //printf("RndSeed: %lld\n", RndSeed[0]);
       setSeed(RndSeed);
-  
       //printf("MCSeed: %lld\n", MinecraftSeed);
       //printf("RndSeed: %lld\n", RndSeed[0]);
-      //printf("RandNum: %d\n", nextInt(10, RndSeed));
-      if (nextInt(10, RndSeed) == 0) {
-	printf("x=%d\tz=%d\tis a slime chunk!\n", x*16, z*16); // Turn to the real location.
+      judge = nextInt(10, RndSeed);
+      //printf("RndSeed: %lld\n", RndSeed[0]);
+      //printf("RandNum: %d\n", judge);
+      if (judge == 0) {
+	printf("x=%d\tz=%d\tis a slime chunk!\n", x, z); // Turn to the real location.
       }
     }
   }
